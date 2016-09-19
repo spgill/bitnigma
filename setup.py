0c228e9b79e9b79ac240fe4d6769da66c45f4129
Samuel P. Gillispie II <spgill@vt.edu>
2016-09-19 12:06:44 -0400

    Published to PyPI!!!

--- setup.py
+++ setup.py
@@ -1,17 +1,38 @@
+import os
+
+import requests
 import setuptools
+
+
+def readme():
+    if os.path.isfile('README.md'):
+        r = requests.post(
+            url='http://c.docverter.com/convert',
+            data={'from': 'markdown', 'to': 'rst'},
+            files={'input_files[]': open('README.md', 'r')}
+        )
+        if r.ok:
+            return r.content.decode()
+        else:
+            return 'ERROR CONVERTING README!'
+    else:
+        return 'LOCAL BUILD! NO README!'
 
 
 setuptools.setup(
     name='bitnigma',
-    version='1.0',
+    version='1.0.1',
     description='Python byte-enabled Enigma-like simulation.',
-    long_description=open('README.md', 'r').read(),
+    long_description=readme(),
     keywords='enigma machine encrypt encryption rotor rotors',
     author='Samuel P. Gillispie II',
     author_email='spgill@vt.edu',
     url='https://github.com/spgill/enigma',
     license='MIT',
     packages=['bitnigma'],
+    install_requires=[
+        'colorama'
+    ],
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
