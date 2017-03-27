41cbfc3f2ebc890b0c84dd3e0ce311769212efc7
Samuel P. Gillispie II <spgill@vt.edu>
2017-03-27 17:19:12 -0400

    Should include the 3.6 tag

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
     ]
 )
